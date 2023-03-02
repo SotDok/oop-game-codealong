948d47ca52a69986ff4450f76995d7000adbe10a
Sotiris Ntokas <ntokas1995@live.com>
2023-03-02 16:17:00 +0100

    (refactor) create Game class

--- js/main.js
+++ js/main.js
@@ -1,76 +1,78 @@
+class Game {
+  constructor() {
+    this.player = null;
+    this.obstaclesArr = []; //will store instances of the class Obstacle
+  }
+  start() {
+    this.player = new Player();
 
+    this.attachEventListeners();
 
-  class Player {
-      constructor() {
+    //create new obstacles
+    setInterval(() => {
+      const myObstacle = new Obstacle();
+      this.obstaclesArr.push(myObstacle);
+    }, 2000);
+
+    //move all obstacles
+    setInterval(() => {
+      this.obstaclesArr.forEach((obstacleInstance) => {
+        obstacleInstance.moveDown();
+      });
+    }, 16);
+  }
+  attachEventListeners() {
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowLeft") {
+        this.player.moveLeft();
+      } else if (e.key === "ArrowRight") {
+        this.player.moveRight();
+      }
+    });
+  }
+}
+
+class Player {
+  constructor() {
     this.positionX = 0;
     this.positionY = 0;
     this.playerElm = document.getElementById("player");
-    }
-      moveLeft() {
+  }
+  moveLeft() {
     this.positionX--;
     this.playerElm.style.left = this.positionX + "vw";
-     }
-      moveRight() {
+  }
+  moveRight() {
     this.positionX++;
     this.playerElm.style.left = this.positionX + "vw";
-      }
   }
+}
 
+class Obstacle {
+  constructor() {
+    this.positionX = 50;
+    this.positionY = 100;
+    this.obstacleElm = null; //will store a dom element
 
-  class Obstacle {
-      constructor(){
-          this.positionX = 50;
-          this.positionY = 100;
-          
-          this.obstacleElm = null;  //will store a dom element
-
-          this.createDomElement();
-      }
-
-      createDomElement(){
-         // step1: create the element:
-        this.obstacleElm = document.createElement('div');
-
-        // step2: add content (ex. innerText) and/or modify attributes 
-        this.obstacleElm.className = "obstacle";
-        this.obstacleElm.style.left = this.positionX + "vw";
-
-        //step3: append to the dom: `parentElm.appendChild()`
-        const boardElm = document.getElementById("board");
-        boardElm.appendChild(this.obstacleElm);
-      }
-      moveDown(){
-        this.positionY--;
-        this.obstacleElm.style.bottom = this.positionY + "vh";
-        
-       
-      }
-  }   
-
-const myPlayer = new Player();
-const obstaclesArr = [];  //will store instances of the class Obstacle
-
-
-// create new obstacles
-setInterval(function(){
-  const myObstacle = new Obstacle();
-  obstaclesArr.push(myObstacle);
-  }, 3000);
-
-
-// move all obstacles
-setInterval(function(){
-    obstaclesArr.forEach( (obstacleInstance) => {
-      obstacleInstance.moveDown();
-    });
-}, 16);
-
-
-// attach event listeners
-document.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowLeft") {
-    myPlayer.moveLeft();
-  } else if (e.key === "ArrowRight") {
-    myPlayer.moveRight();
+    this.createDomElement();
   }
-});
+  createDomElement() {
+    // step1: create the element
+    this.obstacleElm = document.createElement("div");
+
+    // step2: add content (ex. innerText) and/or modify attributes
+    this.obstacleElm.className = "obstacle";
+    this.obstacleElm.style.left = this.positionX + "vw";
+
+    //step3: append to the dom
+    const boardElm = document.getElementById("board");
+    boardElm.appendChild(this.obstacleElm);
+  }
+  moveDown() {
+    this.positionY--;
+    this.obstacleElm.style.bottom = this.positionY + "vh";
+  }
+}
+
+const game = new Game();
+game.start();
